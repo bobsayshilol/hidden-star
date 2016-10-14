76ccc4ae2e1ff47ba0f87620f1c0050f378f524d
Valentin Ochs <a@0au.de>
2016-10-14 12:38:15 +0200

    [feature/audio] Open/close audio

--- audio.c
+++ audio.c
@@ -3,10 +3,15 @@
 
 int audio_setup() {
 	Mix_Init(MIX_INIT_OGG);
+	if(Mix_OpenAudio(48000, AUDIO_U16LSB, 2, 1024) == -1) {
+		fprintf(stderr, "Mix_OpenAudio: %s\n", Mix_GetError());
+		return -1;
+	}
 	return 0;
 }
 
 int audio_quit() {
+	Mix_CloseAudio();
 	Mix_Quit();
 	return 0;
 }
