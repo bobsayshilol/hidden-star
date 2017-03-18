f70fa0172c9884898d602216dc97c933cc1d0c38
Nemoder <redomen@redomen.redomen>
2017-03-17 21:57:50 -0700

    changed volume to 40%

--- audio.c
+++ audio.c
@@ -205,8 +205,8 @@ int audio_setup() {
 	audio_read_config("audio/sfx.tsv");
 
 	//Set initial volume to 80%
-	Mix_VolumeMusic(MIX_MAX_VOLUME - MIX_MAX_VOLUME / 5);
-	Mix_Volume(-1, MIX_MAX_VOLUME - MIX_MAX_VOLUME / 5);
+	Mix_VolumeMusic(MIX_MAX_VOLUME * 0.40);
+	Mix_Volume(-1, MIX_MAX_VOLUME * 0.40);
 	return 0;
 }
 
